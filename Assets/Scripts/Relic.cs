05e5b594898de9dd00794c116df85fdc1fb96b2a
Khélian Larvet <khelian.L@protonmail.com>
2022-10-20 15:08:40 +0200

    Levels fixed

--- Assets/Scripts/Relic.cs
+++ Assets/Scripts/Relic.cs
@@ -7,14 +7,16 @@ public class Relic : MonoBehaviour
 {
 
 #if UNITY_EDITOR
+    public Transform cameraTransform;
     public Vector3 p1, p2, p3, p4;
     private void OnDrawGizmos()
     {
+        Vector3 pos = cameraTransform.position;
         Gizmos.color = Color.black;
-        Gizmos.DrawLine(p1, p2);
-        Gizmos.DrawLine(p2, p3);
-        Gizmos.DrawLine(p3, p4);
-        Gizmos.DrawLine(p4, p1);
+        Gizmos.DrawLine(p1 + pos, p2 + pos);
+        Gizmos.DrawLine(p2 + pos, p3 + pos);
+        Gizmos.DrawLine(p3 + pos, p4 + pos);
+        Gizmos.DrawLine(p4 + pos, p1 + pos);
     }
 #endif
 }
