12f5d9dd4725b4734b57fb8dc26c3a5b9c39e778
Khélian Larvet <khelian.L@protonmail.com>
2022-10-20 23:07:36 +0200

    Adding Particle System Effect for Entities

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,26 +9,34 @@ public class Player : MonoBehaviour
     public AudioClip jumpSound;
     public AudioClip dashSound;
     public float direction = 1.0f;
+    private PSEffect psEffect;
     private void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         aud = GetComponent<AudioSource>();
+        psEffect = GetComponent<PSEffect>();
     }
     public void MoveLeftRight(float value)
     {
         if (CardDash.DASH_IN_EXECUTION > 0)
         {
-            if(aud && !aud.isPlaying)
-            {
-                aud.clip = dashSound;
-                aud.Play();
-            }
+            // Quand le dash est en cours d'exec
         }
 
         if (CardDash.DASH_IN_EXECUTION <= 0)
         {
+            float tmpDir = value > 0 ? 1.0f : (value < 0 ? -1.0f : direction);
+            if ((tmpDir >= 0) != (direction >= 0))
+            {
+                direction = tmpDir;
+                transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y, transform.localScale.z);
 
-            direction = value > 0 ? 1.0f : (value < 0 ? -1.0f : direction);
+                if (psEffect != null)
+                {
+                    psEffect.Flip();
+                }
+
+            }
             rigid.velocity = new Vector2(value * 7.0f, rigid.velocity.y);
         }
     }
@@ -37,11 +45,57 @@ public class Player : MonoBehaviour
     {
         if (type == Card.CardType.Jump)
         {
+            if (psEffect != null)
+            {
+                psEffect.AddEffect(PSEffect.EffectType.Jump);
+            }
             if (aud)
             {
                 aud.clip = jumpSound;
                 aud.Play();
             }
+        }
+        else if (type == Card.CardType.Dash)
+        {
+            if (psEffect != null)
+            {
+                if (direction >= 0)
+                {
+                    psEffect.AddEffect(PSEffect.EffectType.Dash);
+                }
+                else
+                {
+                    psEffect.AddEffect(PSEffect.EffectType.Dash, false);
+                }
+            }
+            if (aud)
+            {
+                aud.clip = dashSound;
+                aud.Play();
+            }
+        }
+    }
+
+    public void Stop()
+    {
+        StopAllCoroutines();
+        CardDash.DASH_IN_EXECUTION = 0;
+        GetComponent<Rigidbody2D>().gravityScale = ActionSystem.GRAVITY_SCALE;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        direction = 1.0f;
+
+        if (transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (psEffect != null)
+        {
+            psEffect.stopEveryParticle();
+        }
+        if (aud)
+        {
+            aud.Stop();
         }
     }
 }
